cd50684255684b268a3553b1f56464332107ff2e
Alamin <mdalaminhossain573190@gmail.com>
2024-03-23 09:21:33 +0600

    loader part

--- src/componants/User/User.jsx
+++ src/componants/User/User.jsx
@@ -1,9 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import SingleUser from "../SingleUser/SingleUser";
+import './User.css'
+
 
 
 const User = () => {
+    const users = useLoaderData()
+    console.log(User)  
     return (
         <div>
-            <h2>this is user part</h2>
+            <h2>Users:{users.length}</h2>
+            <p>this is user part</p>
+            
+            <div className="User">
+                {
+                  users.map(user => <SingleUser key={user.id} user={user}></SingleUser>) 
+                }
+            </div>
+            
         </div>
     );
 };
